2ff42157bf65d7d22d116faeaa47a430eb234420
GroundDev <27danny@naver.com>
2022-06-09 15:35:56 +0900

    [feat] #40 : Color Asset 설정

---
+++ CleanFish/CleanFish/Color+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extension.swift
+//  CleanFish
+//
+//  Created by KimJS on 2022/06/09.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let homeBlue = Color("homeBlue")
+    static let primaryBlue = Color("primaryBlue")
+    static let secondaryBlue = Color("secondaryBlue")
+    static let textGray = Color("textGray")
+}
